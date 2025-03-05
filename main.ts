4ceb09803d50e5d9ce9934a468a64f39cb785cdb
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2025-03-05 14:05:06 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,17 +3,15 @@ let gyro_x = 0
 let gyro_y = 0
 let gyro_z = 0
 let range = SuperBit.RGB_Program().range(0, 3)
-SuperBit.RGB_Program().showRainbow(1, 360)
-SuperBit.Music(SuperBit.enMusic.ode)
-basic.pause(2000)
+SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Orange))
+basic.pause(500)
+SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Yellow))
+basic.pause(500)
+SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Purple))
+basic.pause(500)
 basic.forever(function () {
-    led.plotBarGraph(
-    gyro_x,
-    255
-    )
+    SuperBit.RGB_Program().showColor(neopixel.hsl(gyro_x, gyro_y, gyro_z))
     gyro_x = input.acceleration(Dimension.X)
     gyro_y = input.acceleration(Dimension.Y)
     gyro_z = input.acceleration(Dimension.Z)
-    SuperBit.RGB_Program().showBarGraph(0, neopixel.hsl(gyro_x, gyro_y, gyro_z))
-    basic.pause(1000)
 })
