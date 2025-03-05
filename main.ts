9bab963c0352cccf0841a24ba6ad5ea61cdaea51
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2025-03-05 15:14:06 +0800

    Adjustable.

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    gyro_x += 5
+})
+input.onButtonPressed(Button.B, function () {
+    gyro_x += -5
+})
 basic.showIcon(IconNames.Chessboard)
-let gyro_x = 0
-let gyro_y = 0
-let gyro_z = 0
+let gyro_x = 120
+let gyro_y = 129
+let gyro_z = 120
 let range = SuperBit.RGB_Program().range(0, 3)
 SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Orange))
 basic.pause(500)
@@ -11,7 +17,4 @@ SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Purple))
 basic.pause(500)
 basic.forever(function () {
     SuperBit.RGB_Program().showColor(neopixel.hsl(gyro_x, gyro_y, gyro_z))
-    gyro_x = input.acceleration(Dimension.X) + 5
-    gyro_y = input.acceleration(Dimension.Y) + 5
-    gyro_z = input.acceleration(Dimension.Z) + 5
 })
